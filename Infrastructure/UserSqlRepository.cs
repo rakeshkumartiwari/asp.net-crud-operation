ce9c7807eeda5499102349dfcb3c164191955c8c
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-05-21 14:48:51 +0530

    using stored procedure

--- Infrastructure/UserSqlRepository.cs
+++ Infrastructure/UserSqlRepository.cs
@@ -11,10 +11,18 @@ namespace Infrastructure
         {
             using (var objConnection = new SqlConnection(ConnectionString))
             {
+
                 objConnection.Open();
-                SqlCommand objCommand = new SqlCommand();
-                objCommand.Connection = objConnection;
-                objCommand.CommandText = "insert into EmployeeTable values('" + user.Id + "','" + user.Name + "','" + user.Gender + "','" + user.State + "')";
+                SqlCommand objCommand = new SqlCommand("sp_Save",objConnection);
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.Parameters.AddWithValue("@EmployeeId", user.Id);
+                objCommand.Parameters.AddWithValue("@Name", user.Name);
+                objCommand.Parameters.AddWithValue("@Gender", user.Gender);
+                objCommand.Parameters.AddWithValue("@State", user.State);
+                
+
+                //objCommand.Connection = objConnection;
+                //objCommand.CommandText = "insert into EmployeeTable values('" + user.Id + "','" + user.Name + "','" + user.Gender + "','" + user.State + "')";
                 objCommand.ExecuteNonQuery();
             }
 
@@ -25,8 +33,11 @@ namespace Infrastructure
             using (var objConnection = new SqlConnection(ConnectionString))
             {
                 objConnection.Open();
+                SqlCommand objCommand = new SqlCommand("sp_GetSingleEmployee", objConnection);
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.Parameters.AddWithValue("@EmployeeId", usertId);
                 DataSet objdataSet = new DataSet();
-                SqlDataAdapter objAdapter = new SqlDataAdapter("select * from EmployeeTable where EmployeeId='"+usertId+"'", objConnection);
+                SqlDataAdapter objAdapter = new SqlDataAdapter(objCommand);
                 objAdapter.Fill(objdataSet);
                 return objdataSet;
             }
@@ -36,8 +47,10 @@ namespace Infrastructure
             using (var objConnection = new SqlConnection(ConnectionString))
             {
                 objConnection.Open();
+                SqlCommand objCommand = new SqlCommand("sp_GetEmployees", objConnection);
+                objCommand.CommandType = CommandType.StoredProcedure;
                 DataSet objdataSet = new DataSet();
-                SqlDataAdapter objAdapter = new SqlDataAdapter("select * from EmployeeTable", objConnection);
+                SqlDataAdapter objAdapter = new SqlDataAdapter(objCommand);
                 objAdapter.Fill(objdataSet);
                 return objdataSet;
             }
@@ -48,10 +61,15 @@ namespace Infrastructure
             using (var objConnection = new SqlConnection(ConnectionString))
             {
                 objConnection.Open();
-                SqlCommand objCommand = new SqlCommand();
-                objCommand.Connection = objConnection;
-                objCommand.CommandText = "update EmployeeTable set Name='" + user.Name + "',Gender='" + user.Gender +
-                                         "',State='" + user.State + "' where EmployeeId='" + user.Id + "'";
+                SqlCommand objCommand = new SqlCommand("sp_Update",objConnection);
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.Parameters.AddWithValue("@EmployeeId", user.Id);
+                objCommand.Parameters.AddWithValue("@Name", user.Name);
+                objCommand.Parameters.AddWithValue("@Gender", user.Gender);
+                objCommand.Parameters.AddWithValue("@State", user.State);
+                //objCommand.Connection = objConnection;
+                //objCommand.CommandText = "update EmployeeTable set Name='" + user.Name + "',Gender='" + user.Gender +
+                //                         "',State='" + user.State + "' where EmployeeId='" + user.Id + "'";
                 objCommand.ExecuteNonQuery();
             }
         }
@@ -61,10 +79,13 @@ namespace Infrastructure
             using (var objConnection = new SqlConnection(ConnectionString))
             {
                 objConnection.Open();
-                SqlCommand objCommand = new SqlCommand();
-                objCommand.Connection = objConnection;
-                objCommand.CommandText = "delete EmployeeTable where EmployeeId='" + userId +
-                "'";
+                SqlCommand objCommand = new SqlCommand("sp_Delete", objConnection);
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.Parameters.AddWithValue("@EmployeeId", userId);
+                //SqlCommand objCommand = new SqlCommand();
+                //objCommand.Connection = objConnection;
+                //objCommand.CommandText = "delete EmployeeTable where EmployeeId='" + userId +
+                //"'";
                 objCommand.ExecuteNonQuery();
             }
         }
